caeda9d09ba4772774ce318684bc457ab85e1490
Mikal Morello <mikal_morello@harvard.edu>
2021-03-10 08:34:06 -0500

    React APP initial build, add Firebase Hosting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./images/squid.png" alt="Squid Vector Illustration." />
+        <h1> Squiiids Id Finder coming soon...</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
